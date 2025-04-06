287d59089b79110731f6a9ad4c17d5537803c08e
Claude White <2260114@cegep-heritage.qc.ca>
2025-04-05 21:05:15 -0400

    Initial Commit

---
+++ BiteClub/BiteClubApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiteClubApp.swift
+//  BiteClub
+//
+//  Created by Claude White on 2025-04-05.
+//
+
+import SwiftUI
+
+@main
+struct BiteClubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
