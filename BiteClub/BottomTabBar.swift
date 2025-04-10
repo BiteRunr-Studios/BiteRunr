96a1d60142733206cef178912925e4702902ad12
Ryan Somers <2237815@cegep-heritage.qc.ca>
2025-04-10 11:25:51 -0400

    Some UI improvements. Sheets and bottom nav bar.

---
+++ BiteClub/BottomTabBar.swift
@@ -0,0 +1,33 @@
+//
+//  BottomTabBar.swift
+//  BiteClub
+//
+//  Created by Ryan Somers on 4/10/25.
+//
+
+import SwiftUI
+
+struct BottomTabBar: View {
+    var body: some View {
+        TabView{
+            HomeView()
+                .tabItem{
+                    Label("Home",systemImage: "house.fill")
+                    }
+            AddGroupView()
+                .tabItem{
+                    Label("Add",systemImage: "person.2.badge.plus")
+                    }
+            JoinGroupView()
+                .tabItem{
+                    Label("Join",systemImage: "arrow.trianglehead.branch")
+                    }
+                }
+                }
+       
+    
+}
+
+#Preview {
+    BottomTabBar()
+}
---
+++ BiteClub/JoinGroupView.swift
@@ -0,0 +1,18 @@
+//
+//  JoinGroupView.swift
+//  BiteClub
+//
+//  Created by Ryan Somers on 4/10/25.
+//
+
+import SwiftUI
+
+struct JoinGroupView: View {
+    var body: some View {
+        Text("Join a group")
+    }
+}
+
+#Preview {
+    JoinGroupView()
+}
