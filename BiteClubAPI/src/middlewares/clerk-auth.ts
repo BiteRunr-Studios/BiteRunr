20f3fcde72a2d2a3c04d128867cde235a032b5b5
Claude-White <chitin.skit.0t@icloud.com>
2025-04-12 19:00:30 -0400

    Commented out auth while developing. To be re-enabled for deployment.

--- BiteClubAPI/src/middlewares/clerk-auth.ts
+++ BiteClubAPI/src/middlewares/clerk-auth.ts
@@ -3,39 +3,43 @@ import { verifyToken } from "@clerk/backend";
 import * as HttpStatusCodes from "stoker/http-status-codes";
 import env from "@/env";
 
+// export const authMiddleware: MiddlewareHandler = async (c, next) => {
+//     const authHeader = c.req.header("Authorization");
+//     c.json;
+//     if (!authHeader?.startsWith("Bearer ")) {
+//         return c.json(
+//             {
+//                 message: "Unauthorized: Missing token",
+//             },
+//             HttpStatusCodes.UNAUTHORIZED
+//         );
+//     }
+
+//     const token = authHeader.replace("Bearer ", "");
+//     try {
+//         const verifiedToken = await verifyToken(token, {
+//             secretKey: env.CLERK_SECRET_KEY,
+//             // authorizedParties: [
+//             //     "com.RunrStudios.BiteRunr",
+//             //     "http://localhost:3000",
+//             // ], // match bundle ID or url
+//         });
+
+//         console.log(verifiedToken.sub);
+//         // Store user ID in context for later use
+//         c.set("userId", verifiedToken.sub);
+//         await next();
+//     } catch (err) {
+//         console.error("Token verification failed:", err);
+//         return c.json(
+//             {
+//                 message: "Unauthorized: Missing token",
+//             },
+//             HttpStatusCodes.UNAUTHORIZED
+//         );
+//     }
+// };
+
 export const authMiddleware: MiddlewareHandler = async (c, next) => {
-    const authHeader = c.req.header("Authorization");
-    c.json;
-    if (!authHeader?.startsWith("Bearer ")) {
-        return c.json(
-            {
-                message: "Unauthorized: Missing token",
-            },
-            HttpStatusCodes.UNAUTHORIZED
-        );
-    }
-
-    const token = authHeader.replace("Bearer ", "");
-    try {
-        const verifiedToken = await verifyToken(token, {
-            secretKey: env.CLERK_SECRET_KEY,
-            // authorizedParties: [
-            //     "com.RunrStudios.BiteRunr",
-            //     "http://localhost:3000",
-            // ], // match bundle ID or url
-        });
-
-        console.log(verifiedToken.sub);
-        // Store user ID in context for later use
-        c.set("userId", verifiedToken.sub);
-        await next();
-    } catch (err) {
-        console.error("Token verification failed:", err);
-        return c.json(
-            {
-                message: "Unauthorized: Missing token",
-            },
-            HttpStatusCodes.UNAUTHORIZED
-        );
-    }
+    await next();
 };
