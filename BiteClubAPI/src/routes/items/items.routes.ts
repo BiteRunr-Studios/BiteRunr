e4ea1b8bb67f45d65668ac35203cf0d3303197ac
pierre-badra <PierreBadra@outlook.com>
2025-04-21 21:19:58 -0400

    Added items routes

---
+++ BiteClubAPI/src/routes/items/items.routes.ts
@@ -0,0 +1,138 @@
+import { createRoute } from "@hono/zod-openapi";
+import {
+    jsonContent,
+    jsonContentOneOf,
+    jsonContentRequired,
+} from "stoker/openapi/helpers";
+import * as HttpStatusCodes from "stoker/http-status-codes";
+import { z } from "zod";
+import {
+    insertItemSchema,
+    patchItemSchema,
+    selectItemSchema,
+} from "@/db/schema/items";
+import { createErrorSchema, IdUUIDParamsSchema } from "stoker/openapi/schemas";
+import { notFoundSchema } from "@/lib/constants";
+import { authMiddleware } from "@/middlewares/clerk-auth";
+
+const tags = ["Items"];
+
+export const list = createRoute({
+    path: "/items",
+    method: "get",
+    tags,
+    security: [{ Bearer: [] }],
+    middleware: [authMiddleware] as const,
+    responses: {
+        [HttpStatusCodes.OK]: jsonContent(
+            z.array(selectItemSchema),
+            "List of items"
+        ),
+    },
+});
+
+export const create = createRoute({
+    path: "/items",
+    method: "post",
+    tags,
+    security: [{ Bearer: [] }],
+    middleware: [authMiddleware] as const,
+    request: {
+        body: jsonContentRequired(insertItemSchema, "Create an item"),
+    },
+    responses: {
+        [HttpStatusCodes.OK]: jsonContent(
+            selectItemSchema,
+            "Create an item"
+        ),
+        [HttpStatusCodes.UNPROCESSABLE_ENTITY]: jsonContent(
+            createErrorSchema(insertItemSchema),
+            "Validation error(s)"
+        ),
+    },
+});
+
+export const getOne = createRoute({
+    path: "/items/{id}",
+    method: "get",
+    tags,
+    security: [{ Bearer: [] }],
+    middleware: [authMiddleware] as const,
+    request: {
+        params: IdUUIDParamsSchema,
+    },
+    responses: {
+        [HttpStatusCodes.OK]: jsonContent(
+            selectItemSchema,
+            "Item by Id"
+        ),
+        [HttpStatusCodes.NOT_FOUND]: jsonContent(
+            notFoundSchema,
+            "Item not found"
+        ),
+        [HttpStatusCodes.UNPROCESSABLE_ENTITY]: jsonContent(
+            createErrorSchema(IdUUIDParamsSchema),
+            "Invalid Id error"
+        ),
+    },
+});
+
+export const patch = createRoute({
+    path: "/items/{id}",
+    method: "patch",
+    tags,
+    security: [{ Bearer: [] }],
+    middleware: [authMiddleware] as const,
+    request: {
+        params: IdUUIDParamsSchema,
+        body: jsonContentRequired(patchItemSchema, "Update an Item"),
+    },
+    responses: {
+        [HttpStatusCodes.OK]: jsonContent(
+            selectItemSchema,
+            "Update an Item"
+        ),
+        [HttpStatusCodes.NOT_FOUND]: jsonContent(
+            notFoundSchema,
+            "Item not found"
+        ),
+        [HttpStatusCodes.UNPROCESSABLE_ENTITY]: jsonContentOneOf(
+            [
+                createErrorSchema(patchItemSchema),
+                createErrorSchema(IdUUIDParamsSchema),
+            ],
+            "Validation error(s)"
+        ),
+    },
+});
+
+export const remove = createRoute({
+    path: "/items/{id}",
+    method: "delete",
+    tags,
+    security: [{ Bearer: [] }],
+    middleware: [authMiddleware] as const,
+    request: {
+        params: IdUUIDParamsSchema,
+    },
+    responses: {
+        [HttpStatusCodes.OK]: jsonContent(
+            selectItemSchema,
+            "Deleted item"
+        ),
+        [HttpStatusCodes.NOT_FOUND]: jsonContent(
+            notFoundSchema,
+            "Item not found"
+        ),
+        [HttpStatusCodes.UNPROCESSABLE_ENTITY]: jsonContent(
+            createErrorSchema(IdUUIDParamsSchema),
+            "Invalid Id error"
+        ),
+    },
+});
+
+export type ListRoute = typeof list;
+export type CreateRoute = typeof create;
+export type GetOneRoute = typeof getOne;
+export type PatchRoute = typeof patch;
+export type RemoveRoute = typeof remove;
