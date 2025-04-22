07350bd9389a9d333bb0dad2b572ba389ce83583
Claude White <chitin.skit.0t@icloud.com>
2025-04-22 18:35:47 -0400

    Added react native project

---
+++ BiteClubRN/app/(tabs)/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function HomeScreen() {
+    return (
+        <SafeAreaView>
+            <Text className="text-2xl font-semibold">Index</Text>
+        </SafeAreaView>
+    );
+}
