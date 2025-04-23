684a4c1d59b1f02b2259170b536a437c5a360c93
Ryan Somers <2237815@cegep-heritage.qc.ca>
2025-04-22 23:17:55 -0400

    added so it uses clerk_id to fetch friends.

--- BiteClubRN/Services/api.ts
+++ BiteClubRN/Services/api.ts
@@ -1,8 +1,7 @@
 const apiUrl = "https://biterunrapi.omniquark.me";
-const clerkId = "user_2vPDRe0u445QpaBCnajlHXWkNOA";
 
-export const getFriends = async () => {
-    const response = await fetch(`${apiUrl}/users/clerk/${clerkId}/friends`);
+export const getFriends = async (userId: string) => {
+    const response = await fetch(`${apiUrl}/users/clerk/${userId}/friends`);
     if (!response.ok) { 
         throw new Error("Failed to fetch friends");
     }
--- BiteClubRN/app/(tabs)/friends.tsx
+++ BiteClubRN/app/(tabs)/friends.tsx
@@ -4,13 +4,15 @@ import useFetch from "@/Services/useFetch";
 import { View, Text, ScrollView, ActivityIndicator, useColorScheme, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Colors } from "@/constants/Colors";
+import { useAuth } from "@clerk/clerk-expo";
 import FriendRow from "@/components/ui/FriendRow";
 
 export default function FriendsScreen() {
+    const { userId } = useAuth();
     const colorScheme = useColorScheme();
     const backgroundColor = Colors[colorScheme ?? "light"].background;
     const textColor = Colors[colorScheme ?? "light"].text;
-    const { data: friends, loading: friendsLoading, error: friendsError } = useFetch(() => getFriends());
+    const { data: friends, loading: friendsLoading, error: friendsError } = useFetch(() => getFriends(userId ?? ""));
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor }}>
             <Text style={{ color: textColor }} className="text-2xl text-center font-semibold">Friends</Text>
