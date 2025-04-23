550346c4d31ed571183148340484dc5e6c470fa2
pierre-badra <PierreBadra@outlook.com>
2025-04-23 00:52:42 -0400

    Added additional properties to Colors object and made theming consistent on both light and dark mode

--- BiteClubRN/constants/Colors.ts
+++ BiteClubRN/constants/Colors.ts
@@ -3,24 +3,29 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const tintColorLight = "#0a7ea4";
-const tintColorDark = "#fff";
+const tintColor = "#FF8800";
 
 export const Colors = {
-    light: {
-        text: "#11181C",
-        background: "#fff",
-        tint: tintColorLight,
-        icon: "#687076",
-        tabIconDefault: "#687076",
-        tabIconSelected: tintColorLight,
-    },
-    dark: {
-        text: "#ECEDEE",
-        background: "#151718",
-        tint: tintColorDark,
-        icon: "#9BA1A6",
-        tabIconDefault: "#9BA1A6",
-        tabIconSelected: tintColorDark,
-    },
+  light: {
+    text: "#8E8E8E",
+    ctaText: "#FFF",
+    background: "#fff",
+    tint: tintColor,
+    icon: "#687076",
+    tabIconDefault: "#687076",
+    tabIconSelected: tintColor,
+    inputFieldBorder: "#E1E1E1",
+    inputFieldErrorColor: "#F93B2B"
+  },
+  dark: {
+    text: "#848484",
+    ctaText: "#FFF",
+    background: "#151718",
+    tint: tintColor,
+    icon: "#9BA1A6",
+    tabIconDefault: "#9BA1A6",
+    tabIconSelected: tintColor,
+    inputFieldBorder: "#848484",
+    inputFieldErrorColor: "#F93B2B"
+  },
 };
