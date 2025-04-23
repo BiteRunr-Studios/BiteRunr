a70736ea09c5653719c162e689f609e1768f3026
pierre-badra <PierreBadra@outlook.com>
2025-04-23 00:53:10 -0400

    Added Spacing object for generic page and component spacing and padding

---
+++ BiteClubRN/constants/Spacing.ts
@@ -0,0 +1,9 @@
+export const Spacing = {
+    formFieldPadding: 16,
+    buttonPadding: 16,
+    buttonIconTextGap: 5, 
+    pagePadding: 18,
+    formFieldGap: 10,
+    formFieldButtonGap: 20,
+    pageTitleMarginBottom: 15
+};
