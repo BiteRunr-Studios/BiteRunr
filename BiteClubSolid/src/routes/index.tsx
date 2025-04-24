dbe2dcbac9b39d2e51794ea8ba093b633c229bd8
Claude White <chitin.skit.0t@icloud.com>
2025-04-24 01:49:09 -0400

    Deleted the Solid project

--- BiteClubSolid/src/routes/index.tsx
+++
@@ -1,9 +0,0 @@
-import ProtectedRoute from "~/components/ProtectedRoute";
-
-export default function Home() {
-    return (
-        <ProtectedRoute>
-            <main class="h-full">test</main>
-        </ProtectedRoute>
-    );
-}
