251d813cf6fe7045c08996e1f170cf09692b102f
Claude White <chitin.skit.0t@icloud.com>
2025-04-25 08:59:17 -0400

    Added getLocations to FetchHelpers.kt

--- BiteRunr/shared/build.gradle.kts
+++ BiteRunr/shared/build.gradle.kts
@@ -31,6 +31,7 @@ kotlin {
 
     sourceSets {
         commonMain.dependencies {
+            implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
             implementation("io.ktor:ktor-client-core:$ktorVersion")
             implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
             implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
