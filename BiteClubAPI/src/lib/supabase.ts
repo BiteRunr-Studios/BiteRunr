5f96e8878eea3b567776ef9453f2816ec5283fd0
Claude White <chitin.skit.0t@icloud.com>
2025-04-29 14:00:32 -0400

    Correctly implemented env vars in env.ts

--- BiteClubAPI/src/lib/supabase.ts
+++ BiteClubAPI/src/lib/supabase.ts
@@ -1,7 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
+import env from "@/env";
 
 // Create a single supabase client for interacting with your database
-export const supabase = createClient(
-  process.env.SUPABASE_URL!,
-  process.env.SUPABASE_ANON_KEY!
-);
+export const supabase = createClient(env.SUPABASE_URL, env.SUPABASE_ANON_KEY);
