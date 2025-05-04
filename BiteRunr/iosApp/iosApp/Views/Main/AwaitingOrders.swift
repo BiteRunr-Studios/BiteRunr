c8e9d6747d44f77788ec12c49f53c188836714cf
Ryan Somers <2237815@cegep-heritage.qc.ca>
2025-05-04 18:56:15 -0400

    changed to fetch based off deployed api

--- BiteRunr/iosApp/iosApp/Views/Main/AwaitingOrders.swift
+++ BiteRunr/iosApp/iosApp/Views/Main/AwaitingOrders.swift
@@ -39,7 +39,7 @@ extension AwaitingOrders {
         errorMessage = nil
         
         do {
-            guard let apiUrl = ProcessInfo.processInfo.environment["API_URL_LOCAL"] else {
+            guard let apiUrl = ProcessInfo.processInfo.environment["API_URL"] else {
                 errorMessage = "API_URL not set"
                 return
             }
