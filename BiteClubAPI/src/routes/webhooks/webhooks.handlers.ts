106772e891a99ce117fd09ffa2b67fb450397870
Claude White <chitin.skit.0t@icloud.com>
2025-05-06 22:49:40 -0400

    Removed webhook routes as they are no longer needed (using polling)

--- BiteClubAPI/src/routes/webhooks/webhooks.handlers.ts
+++
@@ -1,9 +0,0 @@
-import type { CreateRoute } from "./webhooks.routes";
-import type { AppRouteHandler } from "@/lib/types";
-import * as HttpStatusCodes from "stoker/http-status-codes";
-
-export const orderItemsHook: AppRouteHandler<CreateRoute> = async (c) => {
-    const reqData = c.req.valid("json");
-    console.log(reqData);
-    return c.json(reqData, HttpStatusCodes.OK);
-};
