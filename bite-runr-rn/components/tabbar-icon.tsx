50d3f7d4e8e868e3acd071e664d347f23f3201d8
pierre-badra <PierreBadra@outlook.com>
2025-11-06 20:18:16 -0500

    Cleaned up auth

--- bite-runr-rn/components/tabbar-icon.tsx
+++ bite-runr-rn/components/tabbar-icon.tsx
@@ -1,9 +1,8 @@
-import FontAwesome from "@expo/vector-icons/FontAwesome";
-import {Ionicons} from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
 
 export const TabBarIcon = (props: {
-	name: React.ComponentProps<typeof Ionicons>["name"];
-	color: string;
+    name: React.ComponentProps<typeof Ionicons>["name"];
+    color: string;
 }) => {
-	return <Ionicons size={24} style={{ marginBottom: -3 }} {...props} />;
+    return <Ionicons size={24} {...props} />;
 };
