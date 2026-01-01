ecb854361a8006747d2ae7eb33821f81fe0e910b
pierre-badra <PierreBadra@outlook.com>
2026-01-01 16:42:42 -0500

    Cleanup post-merge with main

--- bite-runr-rn/components/tabbar-icon.tsx
+++
@@ -1,9 +0,0 @@
-import FontAwesome from "@expo/vector-icons/FontAwesome";
-import {Ionicons} from "@expo/vector-icons";
-
-export const TabBarIcon = (props: {
-	name: React.ComponentProps<typeof Ionicons>["name"];
-	color: string;
-}) => {
-	return <Ionicons size={24} style={{ marginBottom: -3 }} {...props} />;
-};
